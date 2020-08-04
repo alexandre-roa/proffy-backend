7d6b751db72af64afb0219223c2dba598de87e47
Alexandre <alex_roa@aol.com>
2020-08-04 18:28:50 -0300

    eslint routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,11 @@ import ConnectionsController from './controllers/ConnectionsController';
 const routes = express.Router();
 
 const classesController = new ClassesController();
-const connectionsController = new ConnectionsController()
+const connectionsController = new ConnectionsController();
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
 
-routes.post('/connections', connectionsController.create)
-routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 export default routes;
